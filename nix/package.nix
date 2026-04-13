633b2f420b4941eb2072ef9aed3d6cf4aad74ec1
Linus Ammon <235536459+linusammon@users.noreply.github.com>
2026-04-13 10:49:15 +0200

    feat: add a initial nix flake

---
+++ nix/package.nix
@@ -0,0 +1,54 @@
+{
+  lib,
+  stdenv,
+  meson,
+  ninja,
+  pkg-config,
+  gtk4,
+  libadwaita,
+  tesseract,
+  makeWrapper,
+}:
+stdenv.mkDerivation {
+  pname = "waytator";
+  version = "0.0.0";
+
+  src = lib.fileset.toSource {
+    root = ../.;
+    fileset = lib.fileset.unions [
+      ../src
+      ../meson.build
+      ../LICENSE
+    ];
+  };
+
+  nativeBuildInputs = [
+    meson
+    ninja
+    pkg-config
+    makeWrapper
+  ];
+
+  buildInputs = [
+    gtk4
+    libadwaita
+    tesseract
+  ];
+
+  postFixup = ''
+    wrapProgram $out/bin/waytator \
+      --prefix PATH : ${lib.makeBinPath [ tesseract ]}
+  '';
+
+  mesonFlags = [
+    "--buildtype=release"
+  ];
+
+  meta = {
+    homepage = "https://github.com/faetalize/waytator";
+    description = "A screenshot annotator and lightweight image editor";
+    license = lib.licenses.gpl3Only;
+    platforms = lib.platforms.linux;
+    mainProgram = "waytator";
+  };
+}
